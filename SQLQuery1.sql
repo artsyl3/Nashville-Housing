e42ba47bf81a4361585e3c4ee9020c0c93b17571
artsyl3 <118113234+artStyl3@users.noreply.github.com>
2026-03-06 20:12:36 +0100

    sads

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,30 @@
+--Cleaning data in SQL queries
+--......
+
+-- 1. Standardize data format
+
+SELECT SaleDateConverted, Convert(Date,SaleDate)as date FROM NashvilleHousing
+
+
+
+Alter Table NashvilleHousing
+add SaleDateConverted Date;
+
+update [NashvilleHousing ]
+Set SaleDateConverted = Convert(Date,SaleDate)
+
+
+-- 2. populate property address data
+
 SELECT * FROM NashvilleHousing
-Order by 1;
+Where PropertyAddress is null
+order by ParcelID
+
+SELECT * FROM NashvilleHousing a
+Join NashvilleHousing b
+On a.ParcelID = b.ParcelID
+and a.UniqueID <> b.UniqueID
+
+
+
 
